b4170ac3611f74ee5151e2a32cca77655d094665
kannanjute <41623219+kannanjute@users.noreply.github.com>
2018-09-04 21:47:52 +0530

    Add files via upload

---
+++ TestNGSample/bin/mail/Cli.java
@@ -0,0 +1,21 @@
+package mail;
+
+import java.io.IOException;
+
+@Functionality(module= {"login"},story= {"1234"})
+public class Cli {
+
+	@Functionality(module= {"login"},story= {"1234"})
+	public  void executeJenkinsCLI() throws IOException {
+		// TODO Auto-generated method stub
+		
+		
+System.out.println(System.getProperty("user.dir")+"\\lib");
+System.out.println(System.getProperty("java.class.path"));
+
+String path = "java -jar "+System.getProperty("java.class.path")+"\\lib\\jenkins-cli.jar -s http://10.153.86.127:8080 -auth admin:admin build Sample -p suiteName=regression";
+
+Process p = Runtime.getRuntime().exec(path);
+	}
+
+}
